42ef5b206fc01bbbe83b815a48f964d2115e3a94
Django_hp820 <siabdel58@gmail.com>
2021-11-02 18:55:57 +0100

    create a class cart

--- cart/cart.py
+++ cart/cart.py
@@ -2,4 +2,33 @@
 
 from django.conf import settings
 
-session_di = settings['SESSION_ID_AGE']
+session_di = settings['SESSION_COOKIES_AGE']
+
+class cart(object):
+    def __init__(self, request) -> None:
+        cart  = request.session.get(settings.SESSION_COOKIES_AGE)
+        self.session = request.session
+
+        if not cart :
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        
+        self.cart = cart 
+    
+        
+
+    def add(self, product, quantity=1, update_quantity=False ):
+        product_id  = str(product.id)
+        price = product.price 
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'id': product_id,  'price' : price, 'quantity' : 0 }
+
+        if update_quantity :
+            self.cart[product_id]['quantity'] = quantity
+        else :
+            self.cart[product_id]['quantity'] = quantityself.cart[product_id]['quantity'] + 1
+
+
+
+    def save(self):
+        self.session[session.CART_SESSION_ID] = self.cart
